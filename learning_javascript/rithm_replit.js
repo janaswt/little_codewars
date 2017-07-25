8d7b61799ec729a17c65d68a7f45a9ef8cdf45a7
Kang-Li (Stephen) Cheng <kangli.cheng@gmail.com>
2017-07-25 12:45:41 -0400

    Create rithm_replit.js

---
+++ learning_javascript/rithm_replit.js
@@ -0,0 +1,17 @@
+// #1
+function appendToString(str, chars)
+{
+  var newStr =str+chars
+  return newStr
+}
+
+appendToString("Hello", " World!")
+
+// #2
+function prependToString(str, chars)
+{
+ let newStr 
+ newStr=chars+str
+ return newStr 
+}
+prependToString('awesome','very')
